c61faa0db79713822a8797d98887d61608ab8a12
baris-ux <baris.ock.etudes@gmail.com>
2025-10-21 22:22:11 +0200

    menu

--- apps/mobile/app/home.tsx
+++ apps/mobile/app/home.tsx
@@ -1,21 +1,61 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Well done!</Text>
+      {/* --- le navbar ---- */}
+      <View style={styles.navbar}>
+        <TouchableOpacity>
+          <MaterialIcons name="search" size={30} color="#FFFFFF" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name="group" size={30} color="#FFFFFF" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name="account-circle" size={30} color="#FFFFFF" />
+        </TouchableOpacity>
+      </View>
+
+      {/* --- autre element --- */}
+      <View style={styles.content}>
+        <Text style={styles.text}>Well done!</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Permet au conteneur de prendre tout l'espace disponible
-    justifyContent: 'center', // Centre les éléments verticalement
-    alignItems: 'center', // Centre les éléments horizontalement
-    backgroundColor: '#fff', // Définit la couleur de fond en blanc
+    flex: 1,
+    backgroundColor: '#fff',
   },
+
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    paddingVertical: 12,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+
+  navText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
   text: {
     fontSize: 24,
     fontWeight: 'bold',
